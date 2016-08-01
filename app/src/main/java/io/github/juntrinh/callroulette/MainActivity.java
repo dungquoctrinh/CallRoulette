882f936bd29f590ac391e63eb2fd69e6390da221
Jun Trinh <dungtrinh1993@gmail.com>
2016-08-01 14:00:24 -0700

    first commit

---
+++ app/src/main/java/io/github/juntrinh/callroulette/MainActivity.java
@@ -0,0 +1,15 @@
+package io.github.juntrinh.callroulette;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: Create github repository
+    //TODO: Implement for Twiliio in dependency
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
