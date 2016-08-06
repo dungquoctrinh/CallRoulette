a41f0bc004dc13d303e79d9cd758de23f578c450
Jun Trinh <dungtrinh1993@gmail.com>
2016-08-05 23:59:05 -0700

    Add Http handler

---
+++ app/src/main/java/io/github/juntrinh/callroulette/CallRoulette.java
@@ -0,0 +1,99 @@
+package io.github.juntrinh.callroulette;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.twilio.client.Connection;
+import com.twilio.client.Device;
+import com.twilio.client.Twilio;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kid on 8/4/16.
+ */
+public class CallRoulette implements Twilio.InitListener{
+
+    private Device mDevice;
+    private String TAG = "CallRoulette";
+    private Connection mConnection;
+    private Context mContext;
+
+    /**
+     * The device and connection are Twilio objects;
+     * the device represents your phone and the connection maintains your link to the Twilio server
+     * @param context
+     */
+    public CallRoulette(Context context)
+    {
+        this.mContext = context;
+        Twilio.initialize(context, this);
+    }
+
+    @Override
+    public void onInitialized() {
+        Log.d(TAG, "Twilio SDK is ready");
+        new HttpHandler(){
+            @Override
+            public HttpUriRequest getHttpRequestMethod(){
+                Log.d(TAG, mContext.getString(R.string.app_capability_url));
+                return new HttpGet(mContext.getString(R.string.app_capability_url));
+            }
+            @Override
+            public void onResponse(String token) {
+                mDevice = Twilio.createDevice(token, null);
+                Log.d(TAG, "Capability token: " + token);
+            }
+        }.execute();
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    public void connect()
+    {
+        Map<String, String> parameters = new HashMap<String, String>();
+        mConnection = mDevice.connect(parameters, null);
+        if (mConnection == null)
+            Log.w(TAG, "Failed to create new connection");
+    }
+
+    /**
+     * Ends the connection, retrieves the capability token, and tells the server to ‘hangup’ via a get request.
+     * We’ll only receive the capability token when we’ve authenticated successfully with the server,
+     * and it allows us to sign our communications from the device without exposing our AuthToken.
+     */
+    public void disconnect()
+    {
+        if (mConnection != null) {
+            mConnection.disconnect();
+            mConnection = null;
+        }
+        /** a handler to perform a GET request on the hangup URL
+        in order to tell the server that the client has ‘hung up’ the phone.*/
+        new HttpHandler(){
+            @Override
+            public HttpUriRequest getHttpRequestMethod(){
+                Log.d(TAG, mContext.getString(R.string.app_hangup_url));
+                return new HttpGet(mContext.getString(R.string.app_hangup_url));
+            }
+            @Override
+            public void onResponse(String token) {
+                Log.d(TAG, token);
+            }
+        }.execute();
+    }
+
+    @Override
+    protected void finalize()
+    {
+        if (mDevice != null)
+            mDevice.release();
+    }
+}
--- app/src/main/java/io/github/juntrinh/callroulette/MainActivity.java
+++ app/src/main/java/io/github/juntrinh/callroulette/MainActivity.java
@@ -1,15 +1,51 @@
 package io.github.juntrinh.callroulette;
 
+import android.app.Activity;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
-public class MainActivity extends AppCompatActivity {
-
-    //TODO: Create github repository
-    //TODO: Implement for Twiliio in dependency
+public class MainActivity extends Activity implements View.OnClickListener{
+    private Button callButton;
+    private Chronometer callTime;
+    //helper class to connect to Twilio
+    private CallRoulette phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+        callButton = (Button)this.findViewById(R.id.callButton);
+        callTime = (Chronometer)this.findViewById(R.id.clock_view);
+        callButton.setOnClickListener(this);
+        phone = new CallRoulette(this);
+    }
+
+    protected void initView() {
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onClick(View view) {
+        //whenever a user taps the call button, connect/disconnect a phone line
+        if (view.getId() == R.id.callButton && callButton.getText().toString().equals("Call Someone")) {
+
+            phone.connect();
+            //callsomeoneButton.setBackgroundResource(R.drawable.endcallgradient);
+            callButton.setText("End Call");
+            callTime.setVisibility(view.VISIBLE);
+            callTime.setBase(SystemClock.elapsedRealtime());
+            callTime.start();
+
+        } else if(view.getId() == R.id.callButton && callButton.getText().toString().equals("End Call")) {
+
+            phone.disconnect();
+            callButton.setText("Call Someone");
+            //callButton.setBackgroundResource(R.drawable.callgradient);
+            callTime.setVisibility(view.INVISIBLE);
+            callTime.stop();
+            callTime.setBase(SystemClock.elapsedRealtime());
+        }
     }
 }
